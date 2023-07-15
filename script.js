38827e3368a80cc6ab2f1a16278563e553d1d7e4
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-07-16 05:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function test(){
+	let prom = new prom((resolve) => {
+		setTimeOut(() => {
+			resolve("hello world");
+		}, 1000)
+	});
+
+	promise.then((data) => {
+		let output = document.getElementById("output");
+		output.textContent = data ;
+	})
+}
